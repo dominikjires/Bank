98053bc7004b200e70b2a198da26d52bde314fdd
dominikjires <100781365+dominikjires@users.noreply.github.com>
2023-05-14 14:46:24 +0200

    Switch from MailDev to smtp.gmail.com

---
+++ Bank/src/main/java/com/jires/Bank/app/security/PasswordEncoder.java
@@ -0,0 +1,18 @@
+package com.jires.Bank.app.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+
+    // This is the main function that will be executed when the program is run
+    public static void main(String[] args) {
+        // Create a new instance of the BCryptPasswordEncoder class
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "1234";
+
+        // Encode the raw password using the encoder object
+        String encodedPassword = encoder.encode(rawPassword);
+
+        System.out.println(encodedPassword);
+    }
+}
